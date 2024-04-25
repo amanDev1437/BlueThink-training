2fac1bfafac5b6ef9a44137366637f4eacd6334b
amanDev1437 <amanpratap1437@gmail.com>
2024-04-25 19:42:32 +0530

    25-04

--- Exception/Program1.java
+++ Exception/Program1.java
@@ -1,12 +1,16 @@
-public class Program1{
-    public static void main(String[] args){
-        try{
-            int[] a = new int[10];
-            System.out.println(a[11]);
-        }catch(Exception e){
-            System.out.println("Handled exception"+e);
-        }finally{
-            System.out.println("This is finally block");
+import java.util.Scanner;
+
+public class Program1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 10;
+        try {
+            int result = a / n;
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Please don't input 0");
         }
     }
 }
--- Exception/Program2.java
+++ Exception/Program2.java
@@ -1,13 +1,27 @@
-class Program2{
-    public static void checkEligibility(int age){
-        if(age<18){
-            throw new ArithmeticException("Person is not eligible to vote");
-        }else{
-            System.out.println("Person is eligible to vote");
-        }
-    }
+import java.util.Scanner;
 
-    public static void main(String[] args){
-        checkEligibility(17);
+class Program2 {
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int j = 10;
+        try {
+            int result = j / n;
+            System.out.println(result);
+            try {
+                System.out.println("enter the value of index");
+                int i = sc.nextInt();
+                System.out.println(a[i]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Stay in your limit");
+            }
+        } catch (ArithmeticException e) {
+
+            System.out.println("Please don't enter 0");
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+
     }
 }
